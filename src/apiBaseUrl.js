83906ea559f88b743ab409ef83d328d8f9b678af
fabienholert <fabienholert@gmail.com>
2026-04-16 20:18:51 +0000

    [FIX] Utiliser une URL relative pour l'API

--- src/apiBaseUrl.js
+++ src/apiBaseUrl.js
@@ -1,9 +1,2 @@
-// src/apiBaseUrl.js
-
-// Décommenter pour le développement local
-// const apiBaseUrl = "http://localhost:5000/api";
-
-// URL de l'API en production (déployée sur Render)
-const apiBaseUrl = "https://audit-garantie-vw-api.onrender.com/api";
-
+const apiBaseUrl = "/api";
 export default apiBaseUrl;
