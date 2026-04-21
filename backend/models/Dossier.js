2223b8283c816019ac9e1258ee957068bb3ff7b9
Fabien Holert <fabienholert@example.com>
2026-04-21 22:45:33 +0200

    feat: add dateEntree and dateImpression fields to Dossier model

--- backend/models/Dossier.js
+++ backend/models/Dossier.js
@@ -6,6 +6,8 @@ const DossierSchema = new mongoose.Schema({
   vehicule: { type: String, required: true }, // ex: Peugeot 3008
   immatriculation: { type: String, required: true }, // ex: AB-123-CD
   kilometrage: { type: Number, required: true },
+  dateEntree: { type: Date, required: true },
+  dateImpression: { type: Date },
   descriptionPanne: { type: String },
   prixReparation: { type: Number },
   dateFinGarantie: { type: Date },
